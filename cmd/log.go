84cc6d47ccc57ad488057c277831834b0977ad97
masatora <atarashi.masatora@gmail.com>
2021-04-08 20:12:03 +0900

    Add simple log command

--- cmd/log.go
+++ cmd/log.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -11,8 +14,29 @@ var logCmd = &cobra.Command{
 	Use:   "log",
 	Short: "Show question logs",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("log called")
+		err := runLogCmd(cmd)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, err.Error())
+		}
 	},
+}
+
+func runLogCmd(cmd *cobra.Command) (err error) {
+	home_dir := os.Getenv("HOME")
+	log_path := home_dir + "/.question/logs/HEAD"
+	f, err := os.Open(log_path)
+	if err != nil {
+		return
+	}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		slice := strings.Split(scanner.Text(), " ")
+		fmt.Printf("Question: " + slice[0] + "\n")
+		fmt.Printf("Author: " + slice[1] + "\n")
+		fmt.Printf("Date: " + slice[2] + " " + slice[3] + "\n")
+		fmt.Printf("\n\t" + slice[len(slice)-1] + "\n\n")
+	}
+	return
 }
 
 func init() {
